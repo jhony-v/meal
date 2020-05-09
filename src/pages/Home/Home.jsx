593682cb8febf97fbf9117d98f4fd5444c5d8c12
jhony <jhonyvega24@gmail.com>
2020-05-09 14:58:47 -0500

    add first template Ranking View in the home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,19 +5,24 @@ import HeaderToolbarMain from 'shared/layouts/HeaderToolbarMain';
 import FlexContainer from 'shared/styled/FlexContainer';
 import ViewCategories from './views/ViewCategories';
 import ColumnNavigator from './components/ColumnNavigator';
+import Ranking from './views/Ranking';
 
 function Home() {
   return (
     <Drawer appbar={<HeaderToolbarMain />}>
       <FlexContainer sizeHeight="100%" justify="space-between">
+      
         <ColumnNavigator.Aside>
           <ViewCategories />
         </ColumnNavigator.Aside>
+
         <ColumnNavigator.CenterViewNews>
-        
+            <Ranking/>  
         </ColumnNavigator.CenterViewNews>
+      
         <ColumnNavigator.Aside>
         </ColumnNavigator.Aside>
+      
       </FlexContainer>
     </Drawer>
   )
---
+++ src/pages/Home/views/Ranking.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardSimple from 'shared/components/CardSimple';
+import Text from 'shared/styled/Text';
+
+
+const Ranking = () => {
+    return (
+        <CardSimple>
+            <Text weight >Los mas visitados</Text>
+        </CardSimple>
+    )
+}
+
+export default Ranking;
