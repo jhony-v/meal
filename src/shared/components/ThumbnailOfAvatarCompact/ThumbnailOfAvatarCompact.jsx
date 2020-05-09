0094987dc0281e47db535f1603935d2ded83153f
jhony <jhonyvega24@gmail.com>
2020-05-08 20:43:09 -0500

    define style in the component ThumbnailOfAvatarCompact

--- src/shared/components/ThumbnailOfAvatarCompact/ThumbnailOfAvatarCompact.jsx
+++ src/shared/components/ThumbnailOfAvatarCompact/ThumbnailOfAvatarCompact.jsx
@@ -3,14 +3,17 @@ import FlexContainer from 'shared/styled/FlexContainer';
 import AvatarWallpaper from '../AvatarWallpaper';
 import Text from 'shared/styled/Text';
 import Smooth from 'shared/styled/Smooth';
+import { jsx } from "@emotion/core";
+
+/**@jsx jsx */
 
 const ThumbnailOfAvatarCompact = () => {
   return (
     <FlexContainer align="center">
-      <AvatarWallpaper src="https://cdn.pixabay.com/photo/2020/04/20/20/17/lion-5069703__340.jpg" />
+      <AvatarWallpaper src="https://cdn.pixabay.com/photo/2020/04/20/20/17/lion-5069703__340.jpg" css={{marginRight:10}} />
       <FlexContainer justify="center" flexDir="column">
         <Text weight>Jhony vega</Text>
-        <Smooth>10 de febrero de 2020</Smooth>
+        <Smooth size={.8}>10 de febrero de 2020</Smooth>
       </FlexContainer>
     </FlexContainer>
   )
--- src/shared/styled/Smooth.jsx
+++ src/shared/styled/Smooth.jsx
@@ -1,6 +1,7 @@
-import styled  from "@emotion/styled";
+import styled from "@emotion/styled";
 
 const Smooth = styled.div`
+    font-size:${props => props.size && (props.size + 'rem')};
     color : ${props => props.theme.secondaryColors.primaryTextSmooth};
 `
 
