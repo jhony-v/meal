6242a04decf69e010fe4792605e4c1be56ed31d4
jhony <jhonyvega24@gmail.com>
2020-05-09 15:46:21 -0500

    create view eats most ranking

--- src/pages/Home/views/Ranking.jsx
+++ src/pages/Home/views/Ranking.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import CardSimple from 'shared/components/CardSimple';
 import Text from 'shared/styled/Text';
-
+import Container from 'shared/styled/Container';
+import FlexContainer from 'shared/styled/FlexContainer';
+import ThumbnailsInRanking from '../components/ThumbnailsInRanking';
 
 const Ranking = () => {
     return (
         <CardSimple>
-            <Text weight >Los mas visitados</Text>
+            <Container margin="1em 0">
+                <Text weight >Los mas visitados</Text>
+            </Container>
+            <Container margin="1em">
+                <FlexContainer align="center" justify="space-between">
+                    {Array(4).fill(0).map((current, index) => (
+                        <ThumbnailsInRanking 
+                        key={index}
+                        img={`https://source.unsplash.com/collection/${Math.floor(Math.random() * 1000)}`}
+                        text={"Aji de gallina"} />        
+                    ))}
+                </FlexContainer>
+            </Container>
         </CardSimple>
     )
 }
