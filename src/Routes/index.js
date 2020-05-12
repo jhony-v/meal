6a300f0567797d775a42a4b37f8264d56a9efb52
jhony <jhonyvega24@gmail.com>
2020-05-12 09:40:55 -0500

    fix name routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,13 +1,12 @@
 import React, { lazy, Fragment } from "react";
 import { Route } from "react-router-dom";
 
-
 const Home = lazy(() => import("pages/Home"));
 
-const Routes = () => (
-    <Fragment>
-        <Route exact path="/" component={Home} />
-    </Fragment>
-)
-
-export default Routes;
+export default function () {
+    return (
+        <Fragment>
+            <Route exact path="/" component={Home} />
+        </Fragment>
+    );
+}
