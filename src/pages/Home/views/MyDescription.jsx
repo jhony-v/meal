6255be49e0636f35d8205fc660d63cab1e3ebcef
jhony <jhonyvega24@gmail.com>
2020-05-14 16:59:07 -0500

    integrate media video player in news feed

--- src/pages/Home/views/MyDescription.jsx
+++ src/pages/Home/views/MyDescription.jsx
@@ -4,7 +4,6 @@ import Container from 'shared/styled/Container'
 import FlexContainer from 'shared/styled/FlexContainer'
 import Text from 'shared/styled/Text'
 import Smooth from 'shared/styled/Smooth'
-import VideoPlayer from 'shared/components/VideoPlayer'
 
 function MyDescription() {
     return (
@@ -17,7 +16,6 @@ function MyDescription() {
                 <Smooth size={0.8}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam labore, numquam recusandae qui commodi distinctio consequatur deleniti architecto veniam sit praesentium cumque.</Smooth>
                 </Container>
             </Container>
-            <VideoPlayer src="//player.vimeo.com/external/408416886.hd.mp4?s=33e282afa3e7a14a53213130083de7b7a2327da9&profile_id=174" detailVideo="best food app" />
             <Button>editar</Button>
         </FlexContainer>
     )
--- src/pages/Home/views/NewsPublishedOrdered.jsx
+++ src/pages/Home/views/NewsPublishedOrdered.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Container from 'shared/styled/Container';
 import CardPublishedMain from 'shared/components/CardPublishedMain';
+import VideoPlayer from 'shared/components/VideoPlayer';
 
 // {`https://source.unsplash.com/collection/${Math.floor(Math.random() * 1000)}`}
 function NewsPublishedOrdered() {
     return (
         <Container margin="2em 0 0">
-            {Array(10).fill(0).map((current, index) => (
+            {Array(2).fill(0).map((current, index) => (
                 <Container margin="10px 0 0" key={index}>
                     <CardPublishedMain
-                    srcImg={`https://source.unsplash.com/collection/${Math.floor(Math.random() * 1000)}`}
-                    cardTitle="Jhony Vega | Programmer"
-                    cardDetailTitle="hace 10 horas"
-                    detailContext={"Lorem ipsum dolor sit, amet consectetur aquam, laborum rerum soluta dicta tempore officiis eveniet, libero magnam expe."}>
+                        srcImg={`https://source.unsplash.com/collection/${Math.floor(Math.random() * 1000)}`}
+                        cardTitle="Jhony Vega | Programmer"
+                        cardDetailTitle="hace 10 horas"
+                        detailContext={"Lorem ipsum dolor sit, amet consectetur aquam, laborum rerum soluta dicta tempore officiis eveniet, libero magnam expe."}>
+                        <Container margin="1em 0">
+                            <VideoPlayer src="//player.vimeo.com/external/408416886.hd.mp4?s=33e282afa3e7a14a53213130083de7b7a2327da9&profile_id=174" detailVideo="best food app" />
+                        </Container>
                     </CardPublishedMain>
                 </Container>
             ))}
