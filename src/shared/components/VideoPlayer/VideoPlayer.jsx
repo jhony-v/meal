f1d3164445354a4b89ce2a592862405d5b32f4d7
jhony <jhonyvega24@gmail.com>
2020-05-14 12:03:29 -0500

    add text poster to video player

--- src/shared/components/VideoPlayer/VideoPlayer.jsx
+++ src/shared/components/VideoPlayer/VideoPlayer.jsx
@@ -4,6 +4,7 @@ import FlexContainer from 'shared/styled/FlexContainer';
 import Backdrop from 'shared/styled/Backdrop';
 import { IoIosPlayCircle } from 'react-icons/io';
 import { jsx } from "@emotion/core"
+import Text from 'shared/styled/Text';
 
 /**@jsx jsx */
 
@@ -11,9 +12,10 @@ const VideoPlayer = () => {
   return (
     <FlexContainer css={{ position: "relative" }}>
       <video src="" poster="https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706__340.jpg" width="100%" />
-      <Backdrop opacity={0.6} >
-        <FlexContainer align="center" justify="center" sizeHeight="100%">
+      <Backdrop opacity={0.4} >
+        <FlexContainer flexDir="column" align="center" justify="center" sizeHeight="100%">
           <IoIosPlayCircle color="white" size={50} />
+          <Text>food app</Text>
         </FlexContainer>
       </Backdrop>
     </FlexContainer>
