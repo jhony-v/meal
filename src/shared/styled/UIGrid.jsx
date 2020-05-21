e5e05535212832b8139042e55eed305d11025529
jhony <jhonyvega24@gmail.com>
2020-05-20 20:41:45 -0500

    add UIGrid styled component

---
+++ src/shared/styled/UIGrid.jsx
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
+
+const gridTemplateColumns = (props) => {
+    if (props.repeatColumns) {
+        return css(`
+        grid-template-columns:repeat(${props.repeatColumns},1fr);
+        `)
+    }
+    else if (props.sizeColumns) {
+        return css(`
+        grid-template-columns:${sizeColumns.join(' ')};
+        `)
+    }
+}
+
+const UIGrid = styled.div`
+    display:grid;
+    gap:${props => props.gap && props.gap};
+    ${gridTemplateColumns}
+`
+
+export const UIGridItem = styled.div`
+    grid-column: ${props => props.gridColumn && props.gridColumn};
+    grid-row:${props => props.gridRow && props.gridRow};
+`
+
+export default UIGrid;
