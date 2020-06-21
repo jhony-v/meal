1af6691e0a450eff30ea52a4f7f79c8b1e02e423
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-06-21 08:36:28 -0500

    move all properties css that not change to first line

--- src/shared/styled/UICard.jsx
+++ src/shared/styled/UICard.jsx
@@ -2,8 +2,8 @@ import styled from "@emotion/styled";
 
 export const UICard = styled.div`
     border-radius:10px;
-    background : ${(props) => props.theme.colors.light};
     padding : 10px;
+    background : ${(props) => props.theme.colors.light};
     box-shadow : ${(props) => props.shadow || "0 10px 10px rgba(0,0,0,.01)"}; 
     margin : ${(props) => props.margin || "0 auto"};
     }
--- src/shared/styled/UITextSmooth.jsx
+++ src/shared/styled/UITextSmooth.jsx
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
 
 export const UITextSmooth = styled.div`
+  color: ${(props) => props.theme.colors.grayDark2};
   font-size: ${(props) =>
     props.size
       ? props.theme.fontSize[props.size] + "rem"
       : props.theme.fontSize.regular + "rem"};
-  color: ${(props) => props.theme.colors.grayDark2};
 `;
