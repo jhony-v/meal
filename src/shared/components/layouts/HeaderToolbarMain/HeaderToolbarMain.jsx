4916aa2a26e469d8f78e1e2e3012a63ecb5adcfb
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-07-05 18:10:27 -0500

    Delete logo from HeaderToolbarMain layout

--- src/shared/components/layouts/HeaderToolbarMain/HeaderToolbarMain.jsx
+++ src/shared/components/layouts/HeaderToolbarMain/HeaderToolbarMain.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import scss from "./HeaderToolbarMain.module.scss";
 import { UIContainer } from "shared/styled/UIContainer";
 import { UIFlexContainer } from "shared/styled/UIFlexContainer";
-import { UIAvatar } from "shared/styled/UIAvatar";
 import ChipUsername from "shared/containers/ChipUsername";
 import { UIInput } from "shared/styled/UIInput";
 import HeaderLinksNavigation from "./dependencies/HeaderLinksNavigation";
@@ -11,9 +10,6 @@ function HeaderToolbarMain() {
   return (
     <UIContainer variantColor="layout" className={scss.header}>
       <UIFlexContainer align="center" justify="space-between" sizeWidth="100%">
-        <UIAvatar variant="primary" color="white">
-          S
-        </UIAvatar>
         <UIInput placeholder="Buscar los alimentos..." sizeWidth="30%" />
         <HeaderLinksNavigation />
         <ChipUsername />
