1693e15c739b6cfbc9bb2d3598ef7f0e0bffb810
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-07-05 18:07:31 -0500

    Define props into comment entry dumb component and added into NewsFeedOrdered

--- src/pages/Home/dependencies/NewsFeedOrdered/NewsFeedOrdered.jsx
+++ src/pages/Home/dependencies/NewsFeedOrdered/NewsFeedOrdered.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { UIContainer } from 'shared/styled/UIContainer';
 import FeedCardPulished from 'shared/components/smarts/FeedCardPublished';
+import PrimaryCommentEntry from 'shared/components/dumbs/PrimaryCommentEntry';
 
 function NewsFeedOrdered() {
     return (
@@ -14,6 +15,9 @@ function NewsFeedOrdered() {
                     detail="Lorem ipsum dolor sit, amet consectetur aqum soluta dicta tempore officiis eveniet, libero magnam expe.et consectetur aqum soluta dicta tempore official."
                     detailVideo="food app new"
                     video="//player.vimeo.com/external/408416886.hd.mp4?s=33e282afa3e7a14a53213130083de7b7a2327da9&profile_id=174" 
+                    publishedFooter={
+                        <PrimaryCommentEntry/>
+                    }
                     />
             ))}
         </UIContainer>
--- src/shared/components/dumbs/PrimaryCommentEntry.jsx
+++ src/shared/components/dumbs/PrimaryCommentEntry.jsx
@@ -4,19 +4,29 @@ import { UIInput } from "shared/styled/UIInput";
 import { UIContainer } from "shared/styled/UIContainer";
 import { UIAvatar } from "shared/styled/UIAvatar";
 import { IoIosPaperPlane } from "react-icons/io";
+import { useTheme } from "emotion-theming";
+import PropTypes from "prop-types";
 
-
-function PrimaryCommentEntry(){
+function PrimaryCommentEntry({ placeholder, onKeyUp, onClick }) {
+  const { colors } = useTheme();
   return (
     <UIFlexContainer>
-      <UIInput placeholder="Comentar publicación..." />
-      <UIContainer margin="0 0 0 20px">
-        <UIAvatar>
+      <UIInput placeholder={placeholder} onKeyUp={onKeyUp} />
+      <UIContainer margin="0 0 0 10px">
+        <UIAvatar personalized bgColor={colors.primary} color="white" onClick={onClick}>
           <IoIosPaperPlane />
         </UIAvatar>
       </UIContainer>
     </UIFlexContainer>
   );
+}
+
+PrimaryCommentEntry.propTypes = {
+  placeholder: PropTypes.string,
+};
+
+PrimaryCommentEntry.defaultProps = {
+  placeholder: "Comentar...",
 };
 
 export default PrimaryCommentEntry;
