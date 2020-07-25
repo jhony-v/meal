cf86670ef568b494605e291bc2931e869f8ad272
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-07-25 08:10:10 -0500

    feat : Added a new variant theme to the UIButton component

--- meals/src/pages/Home/Home.jsx
+++ meals/src/pages/Home/Home.jsx
@@ -3,13 +3,14 @@ import scss from "./Home.module.scss";
 import HeaderToolbarMain from 'shared/components/layouts/HeaderToolbarMain';
 import { UIFlexContainer } from 'shared/styled/UIFlexContainer';
 import Drawer from 'shared/components/layouts/Drawer';
+import { UIButton } from 'shared/styled/UIButton';
 
 function Home() {
   return (
     <Drawer appbar={<HeaderToolbarMain />}>
       <UIFlexContainer sizeHeight="100%" justify="space-between" className={scss.drawerBody}>
-        
       </UIFlexContainer>
+      <UIButton variant="outline">jhony</UIButton>
     </Drawer>
   )
 }
--- meals/src/shared/styled/UIButton.jsx
+++ meals/src/shared/styled/UIButton.jsx
@@ -1,25 +1,32 @@
 import styled from "@emotion/styled";
 import { css } from "@emotion/core";
 
+// style of primary button
 const backgroundButton = (props) => {
-  return props.variant === "primary"
-    ? css(`
-            background:${props.theme.colors.primary};
-            color:${props.theme.colors.light};`)
-    : css(`
-            background:${props.theme.colors.grayLight1};
-            color:${props.theme.colors.grayDark3};
-        `);
+  let style = "";
+  let colors = props.theme.colors;
+  switch (props.variant) {
+    case "primary":
+      style = ` background:${colors.primary}; color:${colors.light};`;
+      break;
+    case "outline":
+      style = `color: ${colors.primary}; border:1px solid currentColor; font-weight:bold;`;
+      break;
+    default:
+      style = `background:${colors.grayLight1}; color:${colors.grayDark3};`;
+      break;
+  }
+  return css(`${style}`);
 };
 
 //base button theme
 export const UIButton = styled.button`
   padding: 10px 20px;
   transition: opacity 0.1s;
+  ${backgroundButton}
   border-radius: ${(props) => props.borderRadius || "5px"};
   font-size: ${(props) => props.theme.fontSize.regular};
   width: ${(props) => props.sizeWidth && props.sizeWidth};
-  ${backgroundButton}
   &:hover {
     opacity: 0.8;
   }
