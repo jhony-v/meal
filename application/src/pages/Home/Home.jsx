dfebf39c0408b6fdb190ab6ca44656c7c6338e84
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-08-02 12:26:50 -0500

    feat: Add container UserFollowers

--- application/src/pages/Home/Home.jsx
+++ application/src/pages/Home/Home.jsx
@@ -5,8 +5,7 @@ import { UIFlexContainer } from 'shared/styled/UIFlexContainer';
 import Drawer from 'shared/components/layouts/Drawer';
 import HomeTrendsContainer from './HomeTrends/HomeTrendsContainer';
 import HomeWrapperAddPostContainer from './HomeWrapperAddPost/HomeWrapperAddPostContainer';
-import ChipUserFollower from 'shared/components/dumbs/ChipUserFollower';
-import { UIContainer } from 'shared/styled/UIContainer';
+import UserFollowersContainer from 'shared/modules/User/UserFollowers/UserFollowersContainer';
 
 function Home() {
   return (
@@ -14,13 +13,7 @@ function Home() {
       <UIFlexContainer sizeHeight="100%" justify="space-between" className={scss.drawerBody}>
         <HomeTrendsContainer/>
         <HomeWrapperAddPostContainer/>
-				<UIContainer>
-					<ChipUserFollower 
-					avatar="https://cdn.pixabay.com/photo/2017/03/17/04/07/beautiful-2150881__340.jpg"
-					username="Mark zuckerberg"
-					description="@CEO in facebook"
-					buttonContent="Follow" />
-				</UIContainer>
+				<UserFollowersContainer/>
       </UIFlexContainer>
     </Drawer>
   )
---
+++ application/src/shared/modules/User/UserFollowers/UserFollowerItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ChipUserFollower from "shared/components/dumbs/ChipUserFollower";
+
+const UserFollowerItem = () => {
+	return (
+		<ChipUserFollower
+			avatar="https://cdn.pixabay.com/photo/2017/03/17/04/07/beautiful-2150881__340.jpg"
+			username="Mark zuckerberg"
+			description="@CEO in facebook"
+			buttonContent="Follow"
+			padding="15px 0"
+		/>
+	);
+};
+
+export default UserFollowerItem;
