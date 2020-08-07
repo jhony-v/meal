947807332cd7f71965e9837b7c87f43e3fa88770
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-08-07 17:42:09 -0500

    feat: Create useToggle hook

--- application/src/pages/Home/Home.jsx
+++ application/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import scss from "./Home.module.scss";
 import HeaderToolbarMain from 'shared/components/layouts/HeaderToolbarMain';
 import { UIFlexContainer } from 'shared/styled/UIFlexContainer';
@@ -9,20 +9,21 @@ import UserFollowersContainer from 'shared/modules/User/UserFollowers/UserFollow
 import { UIContainer } from 'shared/styled/UIContainer';
 import NewsFeedContainer from 'shared/modules/Publication/NewsFeed/NewsFeedContainer';
 import ModalShareLink from 'shared/components/smarts/ModalShareLink/ModalShareLink';
+import useToggle from 'shared/hooks/useToggle';
 
 function Home() {
-	const [ value , alter ] = useState(false);
-  return (
+	const { open , onToggle } = useToggle();
+	return (
     <Drawer appbar={<HeaderToolbarMain />}>
 			<UIFlexContainer 
 			className={scss.drawerBody} 
 			sizeHeight="100%" 
 			justify="space-between"
 			padding="2em 1in 0" >
-			<ModalShareLink visible={value} />
+			<ModalShareLink visible={open} onClose={onToggle} />
         <HomeTrendsContainer/>
 				<UIContainer className={scss.feeds}>
-			<button onClick={() => alter(!value)}>ckecheabke</button>
+			<button onClick={onToggle}>ckecheabke</button>
     	    <HomeWrapperAddPostContainer/>
 					<NewsFeedContainer/>
 				</UIContainer>
